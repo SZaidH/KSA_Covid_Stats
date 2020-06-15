0d55f6873ab6902242df2db18be7920c253284b4
Zaid Hasan <szh619@gmail.com>
2020-06-15 16:17:33 +0300

    Node.js and Express implementation

---
+++ app.js
@@ -0,0 +1,9 @@
+//Importing packages
+const express = require('express');
+const app = express();
+
+//Specifying the port number
+const PORT;
+
+//Starting the server
+app.listen(PORT, () => console.log(`Server listening at port ${PORT}`));
