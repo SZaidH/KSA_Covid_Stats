03a2ae73c942ccdfde15622c8af6ab08f17c4520
Zaid Hasan <szh619@gmail.com>
2020-06-18 18:18:56 +0300

    Fetching External API + Inserting data to DB functionality added

---
+++ api_insert.js
@@ -0,0 +1,59 @@
+// Package to fetch data from an external API (Source: https://github.com/NovelCOVID/API) and saving it to DB 
+const fetch = require('node-fetch');
+const MongoClient = require('mongodb').MongoClient;
+require('dotenv').config();
+
+//MongoDB URL
+const MONGO_URL = process.env.MONGO_URL;
+
+//API URL
+const API_URL = process.env.API_URL;
+
+//Inseting API Data
+async function insertData() {
+  const response = await fetch(API_URL);
+  const api_data = await response.json();
+
+  //Assigning the API data to variables
+  const cases = api_data.cases;
+  const todayCases = api_data.todayCases;
+  const deaths = api_data.deaths;
+  const todayDeaths = api_data.todayDeaths;
+  const recovered = api_data.recovered;
+  const todayRecovered = api_data.todayRecovered;
+  const active = api_data.active;
+  const critical = api_data.critical;
+  const tests = api_data.tests;
+  const population = api_data.population;
+
+  //Establishing MongoDB connection
+  MongoClient.connect(MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true }, (err, db) => {
+    if (err) throw err;
+    const DB_NAME = process.env.DB_NAME;
+    const dbo = db.db(DB_NAME);
+    //Stats Object
+    let statsObj = {
+      updated: new Date(),
+      cases,
+      todayCases,
+      deaths,
+      todayDeaths,
+      recovered,
+      todayRecovered,
+      active,
+      critical,
+      tests,
+      population
+    };
+
+    //Passing the Stats Object to the collection to be saved as a document
+    const COLLECTION_NAME = process.env.COLLECTION_NAME;
+    dbo.collection(COLLECTION_NAME).insertOne(statsObj, (err, res) => {
+      if (err) throw err;
+      console.log("1 document inserted");
+      db.close();
+    });
+  });
+};
+
+module.exports = insertData;
--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 //Importing packages
 const express = require('express');
 const app = express();
+const api_insert = require('./api_insert');
+require('dotenv').config();
 
 //Specifying the port number
-const PORT;
+const PORT = process.env.PORT;
 
 //Starting the server
-app.listen(PORT, () => console.log(`Server listening at port ${PORT}`));
+app.listen(PORT, () => console.log(`Server listening at port ${PORT}!`));
+console.log(api_insert());
+
+app.use(express.static('public'));
